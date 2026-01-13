dca309afb8ee2d4259d0c62b6c1b7f813aa2b864
Temesgen nemie <119539072+temesgen-nemie@users.noreply.github.com>
2026-01-13 15:13:25 +0300

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios';
 import { FlowJson } from '../store/flowStore';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://ussdtool.profilesage.com',
     headers: {
         'Content-Type': 'application/json',
     },
